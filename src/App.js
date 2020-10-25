97115788bc2dc7fa7abaa41291ff4975ea473289
wolfi374 <http37@mail.ru>
2020-10-25 18:49:33 +0300

    Создан компонент RadioInput

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './css/App.scss';
 import SearchInput from './components/search-input';
+import RadioInput from './ui-kit/radio-input'
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <SearchInput />
+        <div className="gender-filter-container">
+          Фильтр по полу
+          <RadioInput
+            text="Все"
+            value="all"
+            name="gender"
+          />
+          <RadioInput
+            text="Только женщины"
+            value="female"
+            name="gender"
+          />
+          <RadioInput
+            text="Только мужчины"
+            value="male"
+            name="gender"
+          />
+        </div>
       </header>
       <main></main>
     </div>
---
+++ src/ui-kit/radio-input/radio-input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './radio-input.scss';
+
+const RadioInput = ({ text, value, name }) => {
+  return (
+    <div className="radio-input-container">
+      <input className="radio-input" id={`radio-input-${value}`} type="radio" name={name} value={value} />
+      <label className="radio-input-label" for={`radio-input-${value}`}>
+        <div className="radio-input-clickbox" />
+        {text}
+      </label>
+    </div>
+  );
+}
+
+export default RadioInput;
