ea17dab51baa560063283081f4d9cd00dc4a55b4
wolfi374 <http37@mail.ru>
2020-10-25 12:56:35 +0300

    Убран лишний код из App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './css/App.scss';
 import SearchInput from './components/search-input';
 
@@ -7,18 +6,6 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <SearchInput />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <main></main>
     </div>
