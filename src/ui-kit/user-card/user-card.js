6d6e61533f1cecc0fe49010dc596ddecc6df793f
wolfi374 <http37@mail.ru>
2020-10-26 20:19:16 +0300

    Переделанно отображение списка инфо о пользователе

--- src/ui-kit/user-card/user-card.js
+++ src/ui-kit/user-card/user-card.js
@@ -17,19 +17,23 @@ const UserCard = ({ userData }) => {
       />
       <div className="user-info-block">
         <span className="user-name">{userData.name.first} {userData.name.last}</span>
-        <dl className="user-info-fields">
-          <dt>Возраст:</dt>
-          <dd> {userData.dob.age} лет</dd>
-          <br />
-          <dt>Пол:</dt>
-          <dd> {userData.gender}</dd>
-          <br />
-          <dt>Адрес:</dt>
-          <dd> {userData.location.country}, {userData.location.city}
-          </dd>
-          <br />
-          <dt>Дата регистрации:</dt>
-          <dd> {getFormattedRegistrationDate()}</dd>
+        <dl className="user-info-fields-container">
+          <div className="user-info-field">
+            <dt>Возраст:</dt>
+            <dd> {userData.dob.age} лет</dd>
+          </div>
+          <div className="user-info-field">
+            <dt>Пол:</dt>
+            <dd> {userData.gender}</dd>
+          </div>
+          <div className="user-info-field">
+            <dt>Адрес:</dt>
+            <dd> {userData.location.country}, {userData.location.city}</dd>
+          </div>
+          <div className="user-info-field">
+            <dt>Дата регистрации:</dt>
+            <dd> {getFormattedRegistrationDate()}</dd>
+          </div>
         </dl>
       </div>
     </div>
